b3ad1e6ccb0a3a4befdebab6c49a66dc702c4ee4
Chedvy-Rizi <c0556734708@gmail.com>
2025-12-02 20:28:03 +0200

    new commit

--- TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04fb24ddb0f43bf6a0a5b88212672a7602ff67fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb60dc48dee576e05b08ac7b01b70072691a30b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
